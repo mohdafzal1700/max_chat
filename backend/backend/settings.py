6bafedbc7897876533a0663297a79119aa592087
mohdafzal1700 <muhammadafsal578@gmail.com>
2025-09-30 06:53:54 +0530

    updated the simple jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,8 +143,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),  # 2 hours
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
 
